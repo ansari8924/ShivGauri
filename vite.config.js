e08663464e7b433ba221924a180348d8b2e17b93
ansari8924 <aiproject8924@gmail.com>
2026-04-09 11:46:03 +0530

    fix: use function for manualChunks

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,12 @@ export default defineConfig({
     chunkSizeWarningLimit: 800,
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom', 'framer-motion'],
-          firebase: ['firebase']
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('firebase')) return 'firebase';
+            if (id.includes('react') || id.includes('framer-motion')) return 'vendor';
+            return 'deps';
+          }
         }
       }
     }
